742ba7e9831266ad64d83af92a07452ec2779bfe
egetahayamaner <84237369+egetahayamaner@users.noreply.github.com>
2023-12-25 22:10:41 +0300

    Slide ve Injured sistemi eklendi

--- Assets/SurvivorControl.cs
+++ Assets/SurvivorControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SurvivorControl : MonoBehaviour
@@ -9,6 +10,7 @@ public class SurvivorControl : MonoBehaviour
     public float crouchedSpeed = 3.0f;
     public float rotationSpeed = 10.0f;
     private Animator survivorAnimator;
+    public int life = 3;
 
     void Start()
     {
@@ -23,17 +25,22 @@ public class SurvivorControl : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput).normalized;
         bool isMoving = movement != Vector3.zero;
-        bool isRunning = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
         bool isCrouched = Input.GetKey(KeyCode.LeftCommand);
         bool isCrouchedWalking =  Input.GetKey(KeyCode.LeftCommand) && Input.GetKey(KeyCode.W);
+        bool isSliding = Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.LeftShift);
+        bool isInjured = life == 1;
 
         // Determine the current speed based on the character's state
         float currentSpeed;
-        if (isCrouchedWalking) {
+        if (isCrouchedWalking) 
+        {
             currentSpeed = crouchedSpeed; // Use the crouched walking speed when crouched walking
-        } else if (isRunning) {
+        } else if (isRunning) 
+        {
             currentSpeed = runSpeed; // Use running speed when running
-        } else {
+        } else 
+        {
             currentSpeed = walkSpeed; // Default to walking speed
         }
 
@@ -51,5 +58,8 @@ public class SurvivorControl : MonoBehaviour
         survivorAnimator.SetBool("WalkToRun", isMoving && isRunning);
         survivorAnimator.SetBool("isCrouched", isCrouched);
         survivorAnimator.SetBool("isCrouchedWalking", isCrouchedWalking);
+        survivorAnimator.SetBool("isSliding", isSliding);
+        survivorAnimator.SetBool("isInjured", isInjured);
+        isInjured = true;
     }
 }
